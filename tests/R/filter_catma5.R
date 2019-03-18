8378c29c84c5827498c4e4e3172d477f69adfab7
yrodill <benbotho@gmail.com>
2019-03-18 09:05:13 +0100

    corrected R script + commentaries

--- tests/R/filter_catma5.R
+++ tests/R/filter_catma5.R
@@ -6,6 +6,7 @@ lineToDelete10 <- c()
 
 threshold <- ncol(X)*0.1
 
+#Remove row with at least 1 missing data
 for(row in 1:nrow(X)){
   for(col in 1:ncol(X)){
     if(X[row,col]==""){
@@ -15,6 +16,10 @@ for(row in 1:nrow(X)){
   }
 }
 
+removedNASample <- X[-lineToDelete,]
+removedNANorm <- X2[-lineToDelete,]
+
+#Remove row if more than 10% missing data for the row
 for(row in 1:nrow(X)){
   nbMiss<-0
   for(col in 1:ncol(X)){
@@ -27,11 +32,8 @@ for(row in 1:nrow(X)){
   }
 }
 
-removedNASample <- X[-lineToDelete,]
-removedNANorm <- X2[-lineToDelete,]
-
-removedNASample10 <- X[-lineToDelete,]
-removedNANorm10 <- X2[-lineToDelete,]
+removedNASample10 <- X[-lineToDelete10,]
+removedNANorm10 <- X2[-lineToDelete10,]
 
 write.table(removedNASample,file="~/internship-Angers/catma5/filtered_catma5_ISampleNorm.txt",row.names=TRUE, col.names=FALSE,sep="\t")
 write.table(removedNANorm,file="~/internship-Angers/catma5/filtered_catma5_IRefNorm.txt",row.names=TRUE, col.names=FALSE,sep="\t")
