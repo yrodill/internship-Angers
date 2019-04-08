606168eb7d1fccdb7cb1059703c5dfaa8d2f7168
yrodill <benbotho@gmail.com>
2019-04-08 09:38:47 +0200

    bash script to get all links list

--- catma5/biological_results/get_all_links.sh
+++ catma5/biological_results/get_all_links.sh
@@ -3,8 +3,8 @@
 #Usage : bash get_all_links.sh [FOLDER] [NUMBER OF LINKS MAX]
 
 #Get all link list from an experience
-
-for i in "biotic" "abiotic" "all" "development" "chemical"
+#"biotic" "abiotic" "all" "development" "chemical"
+for i in "all"
     do
         python process.py $1 ${i} --nLinks $2
     done
